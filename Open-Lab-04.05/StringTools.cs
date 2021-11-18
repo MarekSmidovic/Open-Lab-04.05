941f32ad989d12fdc680102626b4157e1c4cb306
smidovic <msmidovic@gmail.com>
2021-11-18 14:24:55 +0100

    funkcia na znmnozenie pismen

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace Open_Lab_04._05
+﻿namespace Open_Lab_04._05
 {
     public class StringTools
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            int count = orig.Length-1;
+            string vysledok = string.Empty;
+
+            for (int i = 0; i <= count; i++)
+            {
+                vysledok = vysledok + orig[i];
+
+                for (int j = 0; j < n-1; j++)
+                {
+                    vysledok = vysledok + orig[i];
+                }
+            }
+            return vysledok;
         }
     }
 }
+    
+
+
+
+
